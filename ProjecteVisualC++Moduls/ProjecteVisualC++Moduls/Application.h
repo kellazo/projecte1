fe1d9b1d328bcc5ee2032415be48e6dcf264d4f8
kellazo <gilli11519@gmail.com>
2015-03-18 11:14:56 +0100

    SDL game
    ea fer un nou projecte posar les ddl al debugg

--- ProjecteVisualC++Moduls/ProjecteVisualC++Moduls/Application.h
+++ ProjecteVisualC++Moduls/ProjecteVisualC++Moduls/Application.h
@@ -21,21 +21,55 @@ public:
 
 	bool Init()
 	{
+		bool ret = true
+		p2List_item<Module*>* item = list_modules.getFirst();
 
-		p2List_item<Module*>* item = list_modules.start;
-
-		while (item != NULL)
+		while (item != NULL && ret == true)
 		{
 			item->data->Init();
 			item = item->next;
 		}
 
-
-		return true;
+		
+		
+		//TODO: fer que torni false si algun Init torna false, quan torni false pasara que noslatres hem de parar, treure misatge d'error i tancar.
+		return ret;
+		
+		}
+			
 	}
 
-	int Update() { return UPDATE_STOP; }
-	bool CleanUp() { return true; }
+	int Update() { 
+		Update_status ret = UPDATE_CONTINUE;
+
+		p2List_item<Module*>* item = list_modules.getFirst();
+
+		while (item != NULL && ret == UPDATE_CONTINUE)
+		{
+			item->data->Update();
+			item = item->next;
+		}
+
+		//TODO: que retorni els 3 estats i el maeix amb el cleanup
+		//return UPDATE_STOP;
+		return ret;
+	 }
+
+
+	bool CleanUp() { 
+		
+		p2List_item<Module*>* item = list_modules.getLast();
+
+		while (item != NULL)
+		{
+			item->data->CleanUp();
+			item = item->prev;
+		}
+		
+		
+		return true; 
+	
+	}
 
 private:
 
