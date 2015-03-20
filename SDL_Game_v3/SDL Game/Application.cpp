22d44a4ebed23094bd0b89b9a73fdaef513a2fc5
kellazo <gilli11519@gmail.com>
2015-03-20 12:32:15 +0100

    SDL game versio 3 amb el render i texture input
    SDL game versio 3 amb el render i texture input
    comit versio 3

---
+++ SDL_Game_v3/SDL Game/Application.cpp
@@ -0,0 +1,69 @@
+#pragma once
+#include "Application.h"
+
+Application::Application()
+{
+	renderer = new ModuleRender(this);
+	window = new ModuleWindow(this);
+	textures = new ModuleTextures(this);
+	input = new ModuleInput(this);
+
+	AddModule(window);
+	AddModule(renderer);
+	AddModule(textures);
+	AddModule(input);
+}
+
+Application::~Application()
+{
+	delete renderer;
+	delete window;
+	delete textures;
+	delete input;
+}
+
+bool Application::Init()
+{
+	bool ret = true;
+	p2List_item<Module*>* item = list_modules.getFirst();
+
+	while(item != NULL && ret == true)
+	{
+		ret = item->data->Init();
+		item = item->next;
+	}
+
+	return ret;
+}
+
+update_status Application::Update()
+{
+	update_status ret = UPDATE_CONTINUE;
+	p2List_item<Module*>* item = list_modules.getFirst();
+
+	while(item != NULL && ret == UPDATE_CONTINUE)
+	{
+		ret = item->data->Update();
+		item = item->next;
+	}
+
+	return ret;
+}
+
+bool Application::CleanUp()
+{
+	bool ret = true;
+	p2List_item<Module*>* item = list_modules.getLast();
+
+	while(item != NULL && ret == true)
+	{
+		ret = item->data->CleanUp();
+		item = item->prev;
+	}
+	return ret;
+}
+
+void Application::AddModule(Module* mod)
+{
+	list_modules.add(mod);
+}
---
+++ SDL_Game_v3/SDL Game/ModuleInput.cpp
@@ -0,0 +1,48 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleInput.h"
+
+ModuleInput::ModuleInput(Application* app) : Module(app)
+{
+	keyboard = NULL;
+}
+
+// Destructor
+ModuleInput::~ModuleInput()
+{}
+
+// Called before render is available
+bool ModuleInput::Init()
+{
+	LOG("Init SDL input event system");
+	bool ret = true;
+	SDL_Init(0);
+
+	if(SDL_InitSubSystem(SDL_INIT_EVENTS) < 0)
+	{
+		LOG("SDL_EVENTS could not initialize! SDL_Error: %s\n", SDL_GetError());
+		ret = false;
+	}
+
+	return ret;
+}
+
+// Called every draw update
+update_status ModuleInput::Update()
+{
+	SDL_PumpEvents();
+
+	keyboard = SDL_GetKeyboardState(NULL);
+
+	// TODO: que al apretar ESCAPE la aplicacio acabi
+
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleInput::CleanUp()
+{
+	LOG("Quitting SDL input event subsystem");
+	SDL_QuitSubSystem(SDL_INIT_EVENTS);
+	return true;
+}
